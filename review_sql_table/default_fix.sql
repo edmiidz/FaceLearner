428ca13d6d81e10d82c30a4d67dfc53812a11687
Nik Edmiidz <edmiidz@gmail.com>
2020-01-29 17:18:30 -0800

    Starting over with helloworld
    Starting over with helloworld

--- review_sql_table/default_fix.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE medifaces_review MODIFY COLUMN known int(11) NOT NULL DEFAULT 0;
-ALTER TABLE medifaces_review MODIFY COLUMN unknown int(11) NOT NULL DEFAULT 0;
-ALTER TABLE medifaces_review MODIFY COLUMN done boolean NOT NULL DEFAULT FALSE;
